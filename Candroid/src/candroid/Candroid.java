3c52331ab6919df395c60cb7e3c3855b63b21252
MenatalhHossamalden <146476199+MenaTalhHossamAlden@users.noreply.github.com>
2024-08-21 21:12:33 +0300

    Java Hello World Program

---
+++ Candroid/src/candroid/Candroid.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package candroid;
+
+/**
+ *
+ * @author X1-CB
+ */
+public class Candroid {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Next Trend");
+        System.out.println("Trends Never end");
+    }
+    
+}
